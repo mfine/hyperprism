17952d46e3019d71b6beff6aeae8117cf75a390d
Mark Fine <mark.fine@gmail.com>
2014-10-21 03:31:28 -0700

    simplify.

--- Main.hs
+++ Main.hs
@@ -8,27 +8,28 @@ import Data.Maybe
 import Options.Applicative
 import System.Environment (getEnv)
 
-getApiKey :: IO String
+type ApiKey = String
+type Org = String
+
+getApiKey :: IO ApiKey
 getApiKey = getEnv "GITHUB_API_KEY"
 
-parseOrg :: Parser String
+parseOrg :: Parser Org
 parseOrg = strOption $
   short 'o' <> long "org" <> metavar "ORG" <> help "Org to crawl"
 
-withOptions :: (String -> IO ()) -> IO ()
-withOptions f = f =<< execParser
+getOrg :: IO Org
+getOrg = execParser
   (info (helper <*> parseOrg) $ progDesc "Crawl an org's github repos")
 
 main :: IO ()
-main = withOptions f
-  where
-    f :: String -> IO ()
-    f org = do
-      apiKey <- getApiKey
-      possibleRepos <- Github.organizationRepos' (Just (GithubOAuth apiKey)) org
-      case possibleRepos of
-        (Left error)  -> putStrLn $ "Error: " ++ (show error)
-        (Right repos) -> putStrLn $ intercalate "\n\n" $ map formatRepo repos
+main = do
+  apiKey <- getApiKey
+  org <- getOrg
+  possibleRepos <- Github.organizationRepos' (Just (GithubOAuth apiKey)) org
+  case possibleRepos of
+    (Left error)  -> putStrLn $ "Error: " ++ (show error)
+    (Right repos) -> putStrLn $ intercalate "\n\n" $ map formatRepo repos
 
 formatRepo repo =
   (Github.repoName repo) ++ "\t" ++
