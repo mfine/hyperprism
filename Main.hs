a2d5751021510384b8db964012717dec1efd9855
Mark Fine <mark.fine@gmail.com>
2014-10-21 03:11:51 -0700

    a start

--- Main.hs
+++ Main.hs
@@ -1,2 +1,24 @@
+module Main (main) where
+
+import Options.Applicative
+import System.Environment (getEnv)
+
+getApiKey :: IO String
+getApiKey = getEnv "GITHUB_API_KEY"
+
+parseRepo :: Parser String
+parseRepo = strOption $
+  short 'o' <> long "org" <> metavar "ORG" <> help "Org to crawl"
+
+withOptions :: (String -> IO ()) -> IO ()
+withOptions f = f =<< execParser
+  (info (helper <*> parseRepo) $ progDesc "Crawl an org's github repos")
+
 main :: IO ()
-main = putStrLn "hello world"
+main = withOptions f
+  where
+    f :: String -> IO ()
+    f repo = do
+      k <- getApiKey
+      putStrLn k
+      putStrLn repo
---
+++ src/Hyperprism.hs
@@ -0,0 +1,1 @@
+module Hyperprism where
